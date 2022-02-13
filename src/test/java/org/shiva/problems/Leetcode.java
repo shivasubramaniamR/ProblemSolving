e4ce61a7e85aec45f0a3312236d94f0be7ce815e
shivasubramaniamR <shiva.subramaniam85@gmail.com>
2022-02-13 15:08:35 +0530

    added o(n) solution to the two sums problem

--- src/test/java/org/shiva/problems/Leetcode.java
+++ src/test/java/org/shiva/problems/Leetcode.java
@@ -1,6 +1,7 @@
 package org.shiva.problems;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 import org.testng.annotations.Test;
 
@@ -9,11 +10,12 @@ public class Leetcode {
 	@Test
 	public void runner() {
 		
-		int[] arr = {3,3};
+		int[] arr = {2,7,11,15};
 		
 		
-		System.out.println(Arrays.toString(twoSum(arr,6)));
-		
+		System.out.println(Arrays.toString(twoSum(arr,9)));
+		System.out.println(Arrays.toString(twoSumwithOn(arr,9)));
+		System.out.println(Arrays.toString(twoSumsMap(arr,9)));
 	}
 	
 	
@@ -60,5 +62,53 @@ public class Leetcode {
 
 		return indices;
 	}
+	
+	//order of O(n) - will not work
+	
+	public int[] twoSumwithOn(int [] num,int target) {
+		
+		int left = 0;
+		int right = num.length-1;
+		int tempsum;
+		
+		while(left<right) {
+			
+			tempsum = num[left] + num[right];
+			if(tempsum==target) {
+				return new int[] {left,right};
+			}
+			if(tempsum>target)
+				left++;
+			else
+				right--;
+		}
+		
+		
+		return new int[] {-1,-1};
+		
+	}
+	
+	//O(n) - working logic
+	//Input: nums = [2,7,11,15], target = 9
+	
+	public int[] twoSumsMap(int[] num, int target) {
+		
+		HashMap<Integer,Integer> imap = new HashMap<Integer,Integer>();
+		
+		for(int i=0;i<num.length;i++) {
+			
+			if(imap.containsKey(target-num[i])) {
+				return new int[] {imap.get(target-num[i]),i};
+			}else {
+				imap.put(num[i], i);
+			}
+		}
+		
+		
+		return new int[] {-1,-1};
+	}
+	
+	
+	
 
 }
